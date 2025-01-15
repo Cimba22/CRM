a948659d8d32b95962faaa683b4ddf311762e28c
Oleksandra Mala <oleksandra.s.mala@gmail.com>
2025-01-15 22:55:36 +0100

    first commit

---
+++ src/test/java/com/cimba/crma/MasazhAnechkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cimba.crma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasazhAnechkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
