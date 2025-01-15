ce5bb7fef90a4a7dd48c87b5e0649c8359f4d000
Oleksandra Mala <oleksandra.s.mala@gmail.com>
2025-01-15 23:14:23 +0100

    Schedule structure.

---
+++ src/main/java/com/cimba/crma/features/schedule/ScheduleController.java
@@ -0,0 +1,4 @@
+package com.cimba.crma.features.schedule;
+
+public class ScheduleController {
+}
---
+++ src/main/java/com/cimba/crma/features/schedule/mapper/ScheduleMapper.java
@@ -0,0 +1,4 @@
+package com.cimba.crma.features.schedule.mapper;
+
+public class ScheduleMapper {
+}
